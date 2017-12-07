4f7cdd2fb90150cafc481bd5b5cbe52d8c468f37
BartDeCock <mrdee72@gmail.com>
2017-12-07 17:19:29 +0100

    getting ready for tomorrow :D

---
+++ src/main/java/adventofcode/day8/Day8.java
@@ -0,0 +1,12 @@
+package adventofcode.day8;
+
+public class Day8 {
+
+    void calculatePart1(String filename) {
+
+    }
+
+    void calculatePart2(String filename) {
+
+    }
+}
---
+++ src/test/java/adventofcode/day8/Day8Test.java
@@ -0,0 +1,25 @@
+package adventofcode.day8;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Day8Test {
+
+    @Test
+    public void testPart1() throws Exception {
+    }
+
+    @Test
+    public void calculatePart1() throws Exception {
+    }
+
+    @Test
+    public void testPart2() throws Exception {
+    }
+
+    @Test
+    public void calculatePart2() throws Exception {
+    }
+
+}
