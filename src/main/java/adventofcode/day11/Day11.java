a3f327b354272cacb5c1d5add89a2b20e499142e
BartDeCock <mrdee72@gmail.com>
2017-12-10 21:59:11 +0100

    get ready for day11

---
+++ src/main/java/adventofcode/day11/Day11.java
@@ -0,0 +1,10 @@
+package adventofcode.day11;
+
+import adventofcode.util.InputReader;
+
+public class Day11 {
+
+    void calculatePart1(String filename) {
+        new InputReader().getLinesAsString(filename);
+    }
+}
---
+++ src/test/java/adventofcode/day11/Day11Test.java
@@ -0,0 +1,29 @@
+package adventofcode.day11;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Day11Test {
+
+    @Test
+    public void testPart1() {
+
+    }
+
+    @Test
+    public void calculatePart1() {
+
+    }
+
+    @Test
+    public void testPart2() {
+
+    }
+
+    @Test
+    public void calculatePart2() {
+
+    }
+
+}
