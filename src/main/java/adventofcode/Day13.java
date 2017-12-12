8966a2ab2ece7d814dc5b79e0a3aa7e1c55e41e3
BartDeCock <mrdee72@gmail.com>
2017-12-12 16:21:11 +0100

    prepare

---
+++ src/main/java/adventofcode/Day13.java
@@ -0,0 +1,17 @@
+package adventofcode;
+
+import adventofcode.util.InputReader;
+
+public class Day13 {
+
+    int calculatePart1(String filename) {
+        new InputReader().getLinesAsString(filename);
+
+        return 0;
+    }
+
+    int calculatePart2(String filename) {
+
+        return 0;
+    }
+}
